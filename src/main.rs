b3604577f2190a88909fc2a482baa8736f82c0ba
abdelrahmanaman <abdelrahman.bourka1@gmail>
2024-12-09 23:51:23 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-struct Point<T> {
-    x: T,
-    y: T,
-}
+use std::fs;
+
+
 fn main () {
-    let integer_point = Point{x:5, y:5};
-    println!("{}", integer_point.x)
+    let res =read_file_unsafe("test.txt".to_string());
+    println!("{}", res);
+    
+    print!("Hello, world!");
+}
+
+
+fn read_file_unsafe(file_content: String) -> String {
+    let res = fs::read_to_string(file_content);
+    return res.unwrap()
 }
