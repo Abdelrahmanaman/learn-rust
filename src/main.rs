0291895f6404b9489969998caa8d3c950385a8e0
abdelrahmanaman <abdelrahman.bourka1@gmail>
2024-12-09 23:32:25 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-fn main() {
-    let greeting = String::from("hello world");
-
-let char1 = greeting.chars().nth(1);
-
-match char1{
-    Some(c)=> println!("{}",c),
-    None=> println!("Character not found")
+struct Point<T> {
+    x: T,
+    y: T,
 }
+fn main () {
+    let integer_point = Point{x:5, y:5};
+    println!("{}", integer_point.x)
 }
