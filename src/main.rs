77abc954d36ff0382a12bf20259935337b0fc0e1
abdelrahmanaman <abdelrahman.bourka1@gmail>
2024-12-12 22:03:46 +0100

    ex1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use std::collections::HashMap;
 
 fn main () {
-   let mut users: HashMap<String, i32> = HashMap::new();
 
-   users.insert(String::from("Abdelrahman"), 22);
-   users.insert(String::from("Ab"), 25);
-
-   let user1 = users.get("Abdelrahman");
-
-   match user1 {
-       Some (age) => println!("user is {}", age),
-       None => println!("User not found")
-   }
+    println!("Hello App")
 
 
+
+}
+
+
+/*
+Write a function that takes a vector of tuples (each tuple containing a key and a value) and returns a Hashmaps where the keys are the unique keys from the
+input tuples and the values are vectors of all corresponding values associated
+with each key.
+*/
+
+fn return_tuples(tuples: Vec<(String, i32)>) -> HashMap<String, i32> {
+    let mut map: HashMap<String, i32> = HashMap::new();
+    for t in tuples {
+
+            map.insert(t.0, t.1);
+
+    }
+    return map;
 }
