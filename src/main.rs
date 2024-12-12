e4453e962981deea36c3ced0263d71861334b6f6
abdelrahmanaman <abdelrahman.bourka1@gmail>
2024-12-12 21:19:11 +0100

    hashmaps lesson

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
-
+use std::collections::HashMap;
 
 fn main () {
-    let mut vec = Vec::new();
+   let mut users: HashMap<String, i32> = HashMap::new();
 
-    vec.push(1);
-    vec.push(43);
-    vec.push(4);
-    vec.push(3);
-    vec.push(6);
-    let vec2 = return_even(vec);
-    println!("{:?}", vec2);
-}
+   users.insert(String::from("Abdelrahman"), 22);
+   users.insert(String::from("Ab"), 25);
 
-fn return_even(vec: Vec<i32>) -> Vec<i32> {
-    let mut even_vec =  Vec::new();
-    for i in vec{
-        if i % 2 == 0 {
-             even_vec.push(i);
-        }
-    }
-    Vec::new()
+   let user1 = users.get("Abdelrahman");
+
+   match user1 {
+       Some (age) => println!("user is {}", age),
+       None => println!("User not found")
+   }
+
+
 }
